5107fae6410bbc419fb44723b6c8955b8391579f
Yan Ting Zhe <tingzhe@hotmail.com>
2015-02-03 15:42:41 +0800

    modularise 'preprocess.py'

--- preprocess.py
+++ preprocess.py
@@ -4,39 +4,45 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords
 import pandas as pd
 
-df = pd.read_csv('test.csv')
 
-# Clean up data (some categories have weird spacing)
+def preprocess(df):
+    porter_stemmer = PorterStemmer()
 
-# Solves: TypeError: descriptor 'strip' requires a 'str'
-# object but received a 'float'
-df.fillna("",inplace=True) 
+    # Iterate tweets
+    for index, row in df.iterrows():
+        # Retrieve tweet for a particular row
+        tweet = row['content']
 
-df['sentiment'] = df['sentiment'].map(str.strip)
-df['target'] = df['target'].map(str.strip)
+        # Stemming
+        #porter_stemmer.stem(tweet)
 
-porter_stemmer = PorterStemmer()
+        # abbrevation replaced by actual meaning
 
-# Iterate tweets
-for index, row in df.iterrows():
-    tweet = row['content']
-    # Word preprocessing
-    porter_stemmer.stem(tweet)
-    # Tokenise tweet
+
+        # Tokenise
+        df = tokenise(df, tweet)
+
+    # remove 'content' column
+    df = df.drop('content',1)
+
+def tokenise(df, tweet):
+    # Tokenise string (tweet)
     words = nltk.word_tokenize(tweet)  
+    # remove stopwords
     words = [w for w in words if not w in stopwords.words('english')]
-    # bigram
-    # add back to df while +1 for frequency
-    # create column if word exist
+
+    # Unigram (create column if word exist)
     for each_word in words:
         df.loc[index, each_word] = 1
 
-# remove 'content' column
-df = df.drop('content',1)
-
-# TO-DO: remove unmaed column
+    return df
 
 
+def main():
+    df_train = pd.read_csv('fix_train.csv')
+    df_train = preprocess(df_train)
+    df_train.to_csv("preprocess_train.csv", index=False, na_rep="0")
 
-# output edited csv
-df.to_csv("edit_test.csv", na_rep="0")
+
+
+main()
